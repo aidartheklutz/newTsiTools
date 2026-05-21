f62579f9389c253def422950af1f4349fa1b2941
aidartheklutz <aid4r@proton.me>
2026-05-21 22:21:07 +0600

    kingsman quest

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/HomePage/HomePage";
 import GpaCalc from "./pages/GpaCalc/GpaCalc";
 import { FreshmenCalc } from "./pages/GpaCalc/FreshmenCalc";
 import { ExcuseGen } from "./pages/ExcuseGen/ExcuseGen";
+import { SecretCode } from "./pages/SecretCode/SecretCode";
 import NotFound from "./pages/NotFound/NotFound";
 
 // <Route path='' element={<HomePage />} />
@@ -14,6 +15,7 @@ function App() {
       <Route path="gpa" element={<GpaCalc />} />
       <Route path="gpa/freshmen" element={<FreshmenCalc />} />
       <Route path="excusegen" element={<ExcuseGen />} />;
+      <Route path="KINGSMAN" element={<SecretCode />} />;
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
